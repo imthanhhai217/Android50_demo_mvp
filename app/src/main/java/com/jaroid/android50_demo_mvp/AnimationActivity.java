87a9966e472402a59e3f0784fb7b693c50a2af74
jaroid <imthanhhai217@gmail.com>
2022-12-22 21:38:13 +0700

    animation demo

---
+++ app/src/main/java/com/jaroid/android50_demo_mvp/AnimationActivity.java
@@ -0,0 +1,124 @@
+package com.jaroid.android50_demo_mvp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnimationActivity extends AppCompatActivity {
+
+    View vObject;
+    RadioGroup rdListAnimation;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_animation);
+
+        initView();
+    }
+
+    private void initView() {
+        vObject = findViewById(R.id.vObject);
+        rdListAnimation = findViewById(R.id.rdListAnimation);
+
+        rdListAnimation.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rbScale:
+                        startScaleAnimation();
+                        break;
+                    case R.id.rbRotate:
+                        startRotateAnimation();
+                        break;
+                    case R.id.rbSlideIn:
+                        startSlideInAnimation();
+                        break;
+                    case R.id.rbSlideOut:
+                        startSlideOutAnimation();
+                        break;
+                    case R.id.rbSlideDown:
+                        startSlideDownAnimation();
+                        break;
+                    case R.id.rbFadeIn:
+                        startFadeInAnimation();
+                        break;
+                    case R.id.rbFadeout:
+                        startFadeOutAnimation();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+    private void startFadeOutAnimation() {
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out_animation);
+        translate.setFillAfter(true);
+        vObject.startAnimation(translate);
+    }
+
+    private void startFadeInAnimation() {
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_animation);
+//        translate.setFillAfter(true);
+        vObject.startAnimation(translate);
+    }
+
+    private void startSlideOutAnimation() {
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_animation);
+        translate.setFillAfter(true);
+        vObject.startAnimation(translate);
+    }
+
+    private void startSlideInAnimation() {
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_animation);
+        translate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Toast.makeText(AnimationActivity.this, "SlideIn finish", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        vObject.startAnimation(translate);
+    }
+
+    private void startSlideDownAnimation() {
+        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_down_animation);
+        translate.setInterpolator(new AccelerateInterpolator());
+        vObject.startAnimation(translate);
+    }
+
+    private void startRotateAnimation() {
+        Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_animation);
+        vObject.startAnimation(rotate);
+    }
+
+    private void startScaleAnimation() {
+        Animation scale = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_animation);
+        scale.setFillAfter(true);
+        scale.setDuration(500);
+        scale.setStartOffset(500);
+        scale.setInterpolator(new BounceInterpolator());
+//        scale.setRepeatCount(Animation.INFINITE);
+//        scale.setRepeatMode(Animation.REVERSE);
+        vObject.startAnimation(scale);
+    }
+}
--- app/src/main/java/com/jaroid/android50_demo_mvp/MainActivity.java
+++ app/src/main/java/com/jaroid/android50_demo_mvp/MainActivity.java
@@ -2,6 +2,7 @@ package com.jaroid.android50_demo_mvp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity implements LoginView {
 
     private LoginPresenter loginPresenter;
     EditText edtUserName, edtPasswords;
+    Button btnDemo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +21,25 @@ public class MainActivity extends AppCompatActivity implements LoginView {
 
         edtUserName = findViewById(R.id.edtUserName);
         edtPasswords = findViewById(R.id.edtPasswords);
+        btnDemo = findViewById(R.id.btnDemo);
 
         loginPresenter = new LoginPresenter(this);
         findViewById(R.id.btnSignIn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loginPresenter.login(edtUserName.getText().toString(), edtPasswords.getText().toString());
+                btnDemo.setVisibility(View.GONE);
             }
         });
     }
 
     @Override
     public void loginSuccess(String message) {
-        Toast.makeText(this, ""+message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "" + message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void loginFailed(String message) {
-        Toast.makeText(this, ""+message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "" + message, Toast.LENGTH_SHORT).show();
     }
 }
